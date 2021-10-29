559ccd205e2df050d87cff07e37a96d2af8bf471
Gonzalo <100429091@alumnos.uc3m.es>
2021-10-29 20:20:00 +0200

    Los elemntos colocados donde deben

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,12 @@ import Header from './Header';
 import logo from '../images/logoBlanco.png'
 import PhonePreview from './phonePreview';
 import ReadApi from './apiRead';
-import MyUl from './myUl';
 
 function App() {
   let phones = ReadApi();
   return (
     <div className="App">
       <Header name="Phone Catalog" img={logo}></Header>
-      {/* <MyUl items={phones}></MyUl> */}
       <ul className="list-container">
         { !phones ? "cargando..." : phones.map((phone) =>{
           return <PhonePreview 
@@ -20,6 +18,7 @@ function App() {
                   />
           })}
       </ul>
+
     </div>
   );
 }
