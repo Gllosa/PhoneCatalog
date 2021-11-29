af85241533df017225b42b51d71ff384be2ed4f7
Gonzalo Llosá <gonzalollosa@Gonzalos-MacBook-Pro.local>
2021-11-29 16:08:09 +0100

    Añadido error al hacer un bad req en la api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const phones = require('./data.json')
+const {
+  CustomErrorTypes,
+  errorFactory,
+  tagError,
+  handleHttpError
+} = require('error-handler-module')
 
+const badRequest = errorFactory(CustomErrorTypes.BAD_REQUEST)
 
 const getPhone = (id) =>{
   const phone = phones.find((phone) => phone.id === Number(id))
-  return phone ? Promise.resolve(phone) : null
+  return Promise.resolve(phone)
 }
 
 const server = express()               
@@ -14,14 +21,20 @@ const port = process.env.PORT || 5000
 
 server.use(cors())
 
-server.get('/phones/:id', async (req, res) => {
-  const phone = await getPhone(req.params.id)
-  return res.json(phone)
+server.get('/phones/:id', async (req, res, next) => {
+  try{
+    const phone = await getPhone(req.params.id)
+    if (!phone) throw badRequest('ID not found')
+    return res.json(phone)
+  }catch(error){
+    return next(tagError(error))
+  }
 })
 
 server.get('/phones', (req, res) => {
   return res.json(phones)  
 })
 
+server.use(handleHttpError)
 
 server.listen(port, () => console.log(`Runnning on port ${port}`))
